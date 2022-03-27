22592b007415107bbf964cd9f16a8ab56557c136
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2022-03-28 02:26:01 +0800

    Update Commandremove.java

--- src/main/java/com/brian/MobDrop2/Command/items/Commandremove.java
+++ src/main/java/com/brian/MobDrop2/Command/items/Commandremove.java
@@ -21,23 +21,6 @@ public class Commandremove extends mainCommandSystem{
 	
 	@Override
 	public void run(Player player, String commandLabel, Command command, String[] args) throws Exception {
-		ItemStack setitem = player.getInventory().getItemInMainHand();
-		if (!setitem.getType().toString().equals("AIR")) {
-			if(args.length >= 1) {
-				String str = args[0].replaceAll("&", "§").toUpperCase();
-				List<String> msg = DataBase.sql.ItemsAdd(str, setitem);
-				if(msg.isEmpty()) {
-					DataBase.items.put(str, new Itemset(setitem));
-					player.sendMessage("物品增加成功");
-				} else {
-					player.sendMessage("物品增加失敗");
-					player.sendMessage(msg.toString());
-				}
-			} else {
-				player.sendMessage("請輸入物品id");
-			}
-		} else {
-			player.sendMessage(DataBase.fileMessage.getString("Command.HandNoItem"));
-		}
+		
 	}
 }
